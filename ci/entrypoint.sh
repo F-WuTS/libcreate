78424d187c78b34df71efae599b06f4dca96665e
Konstantin Lampalzer <konstantin.lampalzer@twinformatics.at>
2024-09-23 21:13:21 +0200

    cmake: do not install to opt

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -8,3 +8,8 @@ cmake -B /libcreate/build -S /libcreate -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_F
 cmake --build /libcreate/build --config Release
 ctest -C Release --output-on-failure
 cpack --build /libcreate/build -G DEB
+
+debs=(/libcreate/_packages/*.deb)
+cp "${debs[0]}" /tmp/libcreate.deb
+dpkg-deb -R /tmp/libcreate.deb /tmp/libcreate
+tree /tmp/libcreate
