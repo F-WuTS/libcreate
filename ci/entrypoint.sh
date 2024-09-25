5a591cfbbad809c82f1d9370418ae9b01ab18348
Konstantin Lampalzer <konstantin.lampalzer@twinformatics.at>
2024-09-25 21:58:36 +0200

    ci: remove Werror from entrypoint

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cmake -B /libcreate/build -S /libcreate -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS='-Werror'
+cmake -B /libcreate/build -S /libcreate -DCMAKE_BUILD_TYPE=Release
 cmake --build /libcreate/build --config Release
 ctest -C Release --output-on-failure
 cpack --build /libcreate/build -G DEB
